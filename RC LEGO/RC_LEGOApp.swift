50a05ef5bc7184942ace8a86be4171a4cfbccc40
Alex Ciornei <alexstefciornei22@gmail.com>
2024-05-18 12:14:36 +0300

    Initial Commit

---
+++ RC LEGO/RC_LEGOApp.swift
@@ -0,0 +1,17 @@
+//
+//  RC_LEGOApp.swift
+//  RC LEGO
+//
+//  Created by Alex on 18.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct RC_LEGOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
